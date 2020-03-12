8328cb8814b879b1079e89af938029982b2d4680
Gabriel Vande Hei <gfvandehei@gmail.com>
2020-03-12 19:51:13 -0400

    Here is a basic boilerplate for a react app

---
+++ rpicropcost/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class LoginPage extends React.Component{
+    public render(){
+        return (
+            <div className="container">
+                <p>This is the login page</p>
+            </div>
+        );
+    }
+}
